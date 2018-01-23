ed5e01778a833fa435b8cddab854e79ffec1698f
Bryan Lund <bryan.helge@gmail.com>
2018-01-22 18:18:41 -0700

    added methods to controllers

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -1,2 +1,26 @@
 class SubCategoriesController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
 end
