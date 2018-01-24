3d07168ab39de63072037bf191b2e87e17e4199b
Jerel Lillywhite <supelillywhite@gmail.com>
2018-01-23 18:42:18 -0700

    Added more create methods and links to sub categories and categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,9 +2,19 @@ class CategoriesController < ApplicationController
   before_action :get_category, only: [:show, :edit, :update, :destroy]
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+      else
+        format.html { render :new, notice: 'Category was not created' }
+      end
+    end
   end
 
   def edit
@@ -29,8 +39,12 @@ class CategoriesController < ApplicationController
 
   private
 
-  def get_category
-    @category = Category.find(params[:id])
-  end
+    def get_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 
 end
--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -1,9 +1,19 @@
 class SubCategoriesController < ApplicationController
 before_action :get_sub_category, only: [:show]
   def new
+    @sub_category = SubCategory.new
   end
 
   def create
+    @sub_category = SubCategory.new(sub_category_params)
+
+    respond_to do |format|
+      if @sub_category.save
+        format.html { redirect_to @sub_category, notice: 'Sub Category was successfully created.' }
+      else
+        format.html { render :new, notice: 'Sub Category was not created' }
+      end
+    end
   end
 
   def edit
@@ -27,7 +37,11 @@ before_action :get_sub_category, only: [:show]
 
   private
 
-  def get_sub_category
-    @sub_category = SubCategory.find(params[:id])
-  end
+    def get_sub_category
+      @sub_category = SubCategory.find(params[:id])
+    end
+
+    def sub_category_params
+      params.require(:sub_category).permit(:name, :category_id)
+    end
 end
